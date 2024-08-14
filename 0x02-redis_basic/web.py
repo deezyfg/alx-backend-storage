f2bda9c6d1bc266b7e7d70cadafd80825dffcdec
deezyfg <mensahpeter421@gmail.com>
2024-08-14 17:54:52 +0000

    Added/Updated file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -22,7 +22,7 @@ def data_cacher(method: Callable) -> Callable:
         Callable: The wrapped function with caching and tracking.
     """
     @wraps(method)
-    def invoker(url: str) -> str:
+    def invoker(url) -> str:
         """
         Caches the result of the URL fetch and tracks the request count.
 
@@ -32,23 +32,13 @@ def data_cacher(method: Callable) -> Callable:
         Returns:
             str: The content of the URL, either from cache or fetched anew.
         """
-        # Increment the access count for the URL
         redis_store.incr(f'count:{url}')
-
-        # Attempt to get cached result
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-
-        # Fetch the result from the URL if not cached
-        try:
-            result = method(url)
-        except requests.RequestException as e:
-            return f"An error occurred: {e}"
-
-        # Cache the result with an expiration time of 10 seconds
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
-
         return result
     return invoker
 
@@ -64,17 +54,4 @@ def get_page(url: str) -> str:
     Returns:
         str: The content of the URL.
     """
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.text
-
-
-# Example usage (for testing purposes):
-if __name__ == "__main__":
-    test_url = (
-        "http://slowwly.robertomurray.co.uk/delay/3000/url/"
-        "http://www.google.com"
-    )
-    print(get_page(test_url))
-    access_count = redis_store.get(f'count:{test_url}').decode('utf-8')
-    print(f"Access count for {test_url}: {access_count}")
+    return requests.get(url).text
